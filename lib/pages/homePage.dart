4d7c27941168347d835277a53f2ad833b568eb1f
Prabhneet Narula <imailcode@gmail.com>
2022-01-18 08:58:02 +0530

    images added.

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -8,11 +8,76 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late PageController _pageController;
+
+  void _onScroll() {}
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0)..addListener(_onScroll);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: PageView(
-      children: [Container(color: Colors.blue), Container(color: Colors.teal), Container(color: Colors.cyan)],
+      controller: _pageController,
+      children: [
+        makePage(image: 'lib/assets/images/one.jpg'),
+        makePage(image: 'lib/assets/images/two.jpeg'),
+        makePage(image: 'lib/assets/images/three.jpeg'),
+        makePage(image: 'lib/assets/images/four.jpeg'),
+        makePage(image: 'lib/assets/images/five.jpeg')
+      ],
     ));
+  }
+
+  Widget makePage({image}) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                stops: const [
+              0.3,
+              0.9
+            ],
+                colors: [
+              Colors.black.withOpacity(0.8),
+              Colors.black.withOpacity(0.1)
+            ])),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: const [
+                  Text(
+                    "1",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "/4",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
