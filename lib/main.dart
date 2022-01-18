7799270b045f5f6371753738f79b42fef7386fa8
Prabhneet Narula <imailcode@gmail.com>
2022-01-18 07:47:47 +0530

    homePage added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:trip_app_ui/pages/homePage.dart';
 
-void main() {
-  runApp(const MyApp()); 
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: HomePage(),
     );
   }
 }
